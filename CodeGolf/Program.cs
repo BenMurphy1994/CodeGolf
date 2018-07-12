e3c7d303f99e5401445a99baf373b588642367a5
Ben Murphy <benmurphy@redweb.com>
2018-07-12 15:56:52 +0100

    Add project files.

---
+++ CodeGolf/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            bool test = int.TryParse(args[0], out num);
+            Console.WriteLine(test);
+        }
+    }
+}
